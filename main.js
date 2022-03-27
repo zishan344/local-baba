db8dac667e4347ccc4be9cfd68a6fc57e64db3c4
zishan344 <zishanahmed344@gmail.com>
2022-03-27 11:20:08 +0600

    added the local storage done

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("done");
+const election = ["manna", "sanni", "jayed", "jayed", "jayed"];
+// for (const i of election) {
+//   console.log(election);
+// }
+// for (let i = 0; i < election.length; i++) {
+//   if (election[i] == "jayed") {
+//     console.log(i);
+//   }
+// }
+
+let db = {};
+const elc = (item) => {
+  const existing = localStorage.getItem("checka");
+  if (existing) {
+    db = JSON.parse(existing);
+  }
+  if (item in db) {
+    db[item] = db[item] + 1;
+  } else {
+    db[item] = 1;
+  }
+  localStorage.setItem("checka", JSON.stringify(db));
+};
